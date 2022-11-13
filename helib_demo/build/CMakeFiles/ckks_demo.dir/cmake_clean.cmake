238cfd09c2686ed989fe2c4eaa2e50d9df7b0716
jmparlett <parlettjon@gmail.com>
2022-11-13 15:10:29 -0500

    added helib demo

---
+++ helib_demo/build/CMakeFiles/ckks_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ckks_demo.dir/ckks_demo.cpp.o"
+  "CMakeFiles/ckks_demo.dir/ckks_demo.cpp.o.d"
+  "ckks_demo"
+  "ckks_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ckks_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
